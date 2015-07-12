478ce8e70a4343e3f68407cf593fd3ded329d017
angitacct15 <etl.consultant7678@gmail.com>
2015-07-12 05:47:10 -0500

    fixed legend

--- plot4.R
+++ plot4.R
@@ -45,11 +45,15 @@ loadElectricData_01_02022007<-loadElectricData[which(loadElectricData$Date=="1/2
 loadElectricData_01_02022007$Date_Time<-dmy_hms(paste(loadElectricData_01_02022007[,c("Date")] , loadElectricData_01_02022007[,c("Time")]))
 
 ### line plot to png screen and close
-par(cex.lab=0.75)
+png("plot4.png", width = 480, height = 480,type="cairo")
+
+par(cex.lab=0.75) # reduce lables size by 75pc
+# 2X2 grid layout with borders and space at the top for plot title
 par(mfrow = c(2,2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0))
 
 
-png("plot4.png", width = 480, height = 480,type="cairo")
+
+
 
 # plot for global active power vs date_time
 plot(loadElectricData_01_02022007$Date_Time,
@@ -89,8 +93,8 @@ lines(loadElectricData_01_02022007$Date_Time,
       loadElectricData_01_02022007$Sub_metering_2,type="l",col="red")
 lines(loadElectricData_01_02022007$Date_Time,
       loadElectricData_01_02022007$Sub_metering_3,type="l",col="blue")
-# add legend
-legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex = 0.45,bty = "n")
+# add legend, cex to reduced legend by 75pc
+legend("topright",lty=c(1,1,1),col=c("black","red","blue"),legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex = 0.75,bty = "n")
 
 
 # plot of vooltage vs datetime
